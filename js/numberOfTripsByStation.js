7bb75d1046ca80c4af4cd320e1d97c2534117b7e
amit <londhe.amit@gmail.com>
2015-02-18 22:59:38 -0500

    Bar Chart for Top 10 Stations

---
+++ js/numberOfTripsByStation.js
@@ -0,0 +1,340 @@
+"8 Ave & W 31 St",101916
+"E 17 St & Broadway",97180
+"Lafayette St & E 8 St",96544
+"Broadway & E 14 St",80190
+"W 21 St & 6 Ave",80150
+"West St & Chambers St",78158
+"8 Ave & W 33 St",72318
+"Cleveland Pl & Spring St",71758
+"Broadway & E 22 St",71201
+"University Pl & E 14 St",70015
+"W 20 St & 11 Ave",69888
+"Broadway & W 24 St",69840
+"Broadway & W 60 St",67933
+"W 41 St & 8 Ave",66318
+"Greenwich Ave & 8 Ave",64632
+"E 43 St & Vanderbilt Ave",62377
+"Christopher St & Greenwich St",62356
+"Carmine St & 6 Ave",61355
+"Vesey Pl & River Terrace",61151
+"W 18 St & 6 Ave",59714
+"2 Ave & E 31 St",57311
+"W Houston St & Hudson St",56883
+"Pershing Square N",56693
+"W 4 St & 7 Ave S",56215
+"W 22 St & 10 Ave",55183
+"Broadway & W 58 St",54662
+"W 17 St & 8 Ave",54352
+"Centre St & Chambers St",53726
+"W 33 St & 7 Ave",53604
+"1 Ave & E 15 St",52845
+"W 38 St & 8 Ave",52699
+"Barclay St & Church St",52339
+"W 31 St & 7 Ave",52240
+"W 39 St & 9 Ave",52010
+"W 24 St & 7 Ave",51667
+"MacDougal St & Prince St",51367
+"Grand Army Plaza & Central Park S",51346
+"E 39 St & 2 Ave",51040
+"E 32 St & Park Ave",50773
+"W 13 St & 6 Ave",50178
+"Barrow St & Hudson St",49202
+"9 Ave & W 22 St",48976
+"Broadway & W 41 St",48629
+"W 25 St & 6 Ave",48612
+"E 42 St & Vanderbilt Ave",48608
+"Lexington Ave & E 26 St",48405
+"Mercer St & Bleecker St",48350
+"Lexington Ave & E 24 St",47592
+"Great Jones St",46911
+"Greenwich St & Warren St",46861
+"9 Ave & W 14 St",46676
+"E 11 St & 2 Ave",46320
+"Mott St & Prince St",46127
+"W 27 St & 7 Ave",45962
+"South End Ave & Liberty St",45710
+"W 15 St & 7 Ave",45129
+"6 Ave & W 33 St",45062
+"Washington Square E",44837
+"LaGuardia Pl & W 3 St",44098
+"Broadway & W 32 St",43924
+"W 22 St & 8 Ave",43770
+"St Marks Pl & 2 Ave",43454
+"9 Ave & W 45 St",42991
+"Central Park S & 6 Ave",42988
+"Allen St & Rivington St",42976
+"E 10 St & Avenue A",42928
+"W 20 St & 7 Ave",42792
+"W 49 St & 8 Ave",42709
+"West Thames St",42651
+"W Broadway & Spring St",42418
+"W 26 St & 8 Ave",41969
+"E 12 St & 3 Ave",41913
+"12 Ave & W 40 St",41866
+"Broadway & W 49 St",41737
+"E 40 St & 5 Ave",41392
+"9 Ave & W 16 St",41277
+"E 31 St & 3 Ave",40892
+"Allen St & E Houston St",40814
+"E 58 St & 3 Ave",40440
+"E 39 St & 3 Ave",40330
+"E 11 St & 1 Ave",39738
+"5 Ave & E 29 St",39523
+"Washington St & Gansevoort St",39508
+"E 16 St & 5 Ave",39377
+"Broadway & W 29 St",39310
+"E 23 St & 1 Ave",39276
+"8 Ave & W 52 St",39172
+"W 26 St & 10 Ave",39121
+"E 13 St & Avenue A",39013
+"W 43 St & 10 Ave",38964
+"E 25 St & 2 Ave",38959
+"E 4 St & 2 Ave",38945
+"Greenwich St & N Moore St",38776
+"E 3 St & 1 Ave",38612
+"E 7 St & Avenue A",38574
+"E 30 St & Park Ave S",38536
+"W 16 St & The High Line",37876
+"W 56 St & 6 Ave",37773
+"Suffolk St & Stanton St",37590
+"Sullivan St & Washington Sq",37126
+"E 20 St & 2 Ave",37116
+"E 19 St & 3 Ave",36715
+"W 37 St & 10 Ave",36527
+"E 2 St & Avenue B",36207
+"W 45 St & 8 Ave",35983
+"E 16 St & Irving Pl",35434
+"W 42 St & 8 Ave",35345
+"Lawrence St & Willoughby St",35307
+"Broadway & W 55 St",35204
+"Liberty St & Broadway",35005
+"1 Ave & E 30 St",34810
+"W 13 St & 7 Ave",34667
+"Forsyth St & Broome St",34363
+"Canal St & Rutgers St",34228
+"Howard St & Centre St",34173
+"Allen St & Hester St",34035
+"Broadway & W 53 St",33966
+"Elizabeth St & Hester St",33943
+"11 Ave & W 27 St",33876
+"1 Ave & E 18 St",33648
+"Washington Pl & Broadway",33642
+"E 47 St & Park Ave",33268
+"W 45 St & 6 Ave",33251
+"E 6 St & Avenue B",33169
+"W 59 St & 10 Ave",33146
+"W 20 St & 8 Ave",33145
+"St Marks Pl & 1 Ave",33084
+"E 15 St & 3 Ave",32725
+"W 52 St & 11 Ave",32304
+"E 10 St & 5 Ave",32298
+"Bank St & Hudson St",32216
+"Lafayette St & Jersey St",32204
+"E 2 St & 2 Ave",32139
+"Pershing Square S",31985
+"W 51 St & 6 Ave",31960
+"E 33 St & 1 Ave",31939
+"Cliff St & Fulton St",31937
+"11 Ave & W 41 St",31873
+"Broadway & W 51 St",31778
+"E 9 St & Avenue C",31420
+"Mercer St & Spring St",31400
+"9 Ave & W 18 St",31226
+"10 Ave & W 28 St",31186
+"E 14 St & Avenue B",30853
+"2 Ave & E 58 St",30830
+"E 33 St & 5 Ave",30596
+"Watts St & Greenwich St",30263
+"Stanton St & Chrystie St",30106
+"Greenwich Ave & Charles St",30010
+"Broadway & Battery Pl",29812
+"W 37 St & 5 Ave",29518
+"Bayard St & Baxter St",29482
+"MacDougal St & Washington Sq",29183
+"W 29 St & 9 Ave",29067
+"Franklin St & W Broadway",28976
+"E 20 St & FDR Drive",28858
+"FDR Drive & E 35 St",28853
+"E 11 St & Broadway",28640
+"W 47 St & 10 Ave",28315
+"Washington Pl & 6 Ave",28234
+"Bank St & Washington St",27619
+"Lispenard St & Broadway",27176
+"Perry St & Bleecker St",26954
+"Broadway & W 39 St",26864
+"Maiden Ln & Pearl St",26756
+"South St & Gouverneur Ln",26703
+"W 14 St & The High Line",26637
+"E 25 St & 1 Ave",26564
+"W 44 St & 5 Ave",26351
+"Broadway & W 36 St",26344
+"E 51 St & Lexington Ave",26295
+"Grand St & Greene St",26189
+"E 48 St & 3 Ave",25871
+"W 46 St & 11 Ave",25559
+"Rivington St & Chrystie St",25550
+"Centre St & Worth St",25414
+"Park Pl & Church St",25374
+"W 53 St & 10 Ave",24964
+"6 Ave & Canal St",24851
+"E 47 St & 2 Ave",24369
+"Broadway & W 37 St",24355
+"E 45 St & 3 Ave",24279
+"Hudson St & Reade St",24275
+"Forsyth St & Canal St",24152
+"W 11 St & 6 Ave",24140
+"Norfolk St & Broome St",24097
+"Clinton St & Grand St",24004
+"E 53 St & Lexington Ave",23947
+"Warren St & Church St",23871
+"1 Ave & E 44 St",23583
+"W 52 St & 5 Ave",23530
+"Pearl St & Hanover Square",23498
+"S 5 Pl & S 4 St",23374
+"6 Ave & Broome St",23364
+"Fulton St & William St",23276
+"Murray St & West St",23218
+"Reade St & Broadway",22962
+"Division St & Bowery",22728
+"John St & William St",22622
+"Spruce St & Nassau St",22581
+"E 37 St & Lexington Ave",22436
+"E 20 St & Park Ave",22379
+"W 49 St & 5 Ave",22281
+"W 56 St & 10 Ave",22224
+"William St & Pine St",22108
+"E 53 St & Madison Ave",22011
+"Little West St & 1 Pl",22008
+"Metropolitan Ave & Bedford Ave",21910
+"W 52 St & 9 Ave",21734
+"Front St & Maiden Ln",20981
+"E 56 St & 3 Ave",20927
+"E 2 St & Avenue C",20758
+"E 27 St & 1 Ave",20662
+"W 43 St & 6 Ave",20600
+"Joralemon St & Adams St",20452
+"Duane St & Greenwich St",20349
+"E 56 St & Madison Ave",20334
+"South St & Whitehall St",20053
+"York St & Jay St",19977
+"Henry St & Grand St",19865
+"W 13 St & 5 Ave",19570
+"Front St & Washington St",19464
+"Shevchenko Pl & E 6 St",19327
+"Ashland Pl & Hanson Pl",19284
+"E 55 St & 2 Ave",19159
+"W 54 St & 9 Ave",19027
+"W 34 St & 11 Ave",18992
+"E 5 St & Avenue C",18933
+"E 59 St & Sutton Pl",18843
+"Wythe Ave & Metropolitan Ave",18812
+"E 51 St & 1 Ave",18761
+"State St & Smith St",18727
+"St James Pl & Oliver St",18697
+"Pike St & E Broadway",18536
+"DeKalb Ave & Hudson Ave",18253
+"Rivington St & Ridge St",18031
+"E 24 St & Park Ave S",17663
+"Harrison St & Hudson St",17161
+"Clark St & Henry St",17002
+"E 41 St & Madison Ave",16474
+"State St",16331
+"Old Fulton St",16013
+"Old Slip & Front St",16007
+"E 52 St & 2 Ave",15766
+"Madison St & Montgomery St",15521
+"Bialystoker Pl & Delancey St",15457
+"Clinton Ave & Myrtle Ave",15326
+"Dean St & 4 Ave",15288
+"E 43 St & 2 Ave",15250
+"Water - Whitehall Plaza",15229
+"Columbia St & Rivington St",15158
+"Atlantic Ave & Fort Greene Pl",15132
+"Henry St & Atlantic Ave",14967
+"E 47 St & 1 Ave",14873
+"Fulton St & Rockwell Pl",14671
+"Hicks St & Montague St",14667
+"Madison St & Clinton St",14419
+"E 55 St & Lexington Ave",14366
+"Catherine St & Monroe St",14148
+"Church St & Leonard St",14098
+"Cherry St",13990
+"Stanton St & Mangin St",13588
+"E 6 St & Avenue D",13444
+"Atlantic Ave & Furman St",12933
+"Avenue D & E 3 St",12659
+"Broad St & Bridge St",12550
+"St James Pl & Pearl St",12430
+"Pike St & Monroe St",12341
+"Bond St & Schermerhorn St",12216
+"Pearl St & Anchorage Pl",12209
+"Adelphi St & Myrtle Ave",11922
+"Market St & Cherry St",11787
+"Clinton St & Tillary St",11117
+"11 Ave & W 59 St",11002
+"Shevchenko Pl & E 7 St",10926
+"Johnson St & Gold St",10855
+"Cadman Plaza E & Tillary St",10464
+"Fulton St & Waverly Ave",10416
+"Cumberland St & Lafayette Ave",10334
+"Willoughby St & Fleet St",10186
+"Lafayette Ave & Fort Greene Pl",10112
+"Avenue D & E 8 St",10106
+"Jay St & Tech Pl",10028
+"Clinton St & Joralemon St",9991
+"DeKalb Ave & Vanderbilt Ave",9571
+"Laight St & Hudson St",9568
+"S 3 St & Bedford Ave",9399
+"S 4 St & Wythe Ave",9391
+"Willoughby Ave & Hall St",9352
+"Broadway & Berry St",8762
+"DeKalb Ave & S Portland Ave",8573
+"Grand St & Havemeyer St",8404
+"Pitt St & Stanton St",8373
+"Leonard St & Church St",8255
+"Washington Ave & Park Ave",8097
+"Washington Park",7736
+"Duffield St & Willoughby St",7559
+"Bus Slip & State St",7471
+"Henry St & Poplar St",7352
+"Fulton St & Grand Ave",7296
+"Columbia Heights & Cranberry St",7176
+"Clermont Ave & Lafayette Ave",7058
+"Front St & Gold St",7003
+"Lefferts Pl & Franklin Ave",6953
+"Flushing Ave & Carlton Ave",6867
+"Emerson Pl & Myrtle Ave",6389
+"Lafayette Ave & Classon Ave",6304
+"Montague St & Clinton St",6299
+"S Portland Ave & Hanson Pl",6280
+"Willoughby Ave & Walworth St",6099
+"Lafayette Ave & St James Pl",6069
+"Avenue D & E 12 St",5697
+"Kent Ave & S 11 St",5695
+"Lexington Ave & Classon Ave",5432
+"Washington Ave & Greene Ave",5387
+"Carlton Ave & Park Ave",5195
+"Concord St & Bridge St",5192
+"Fulton St & Clermont Ave",5066
+"Monroe St & Bedford Ave",5065
+"Clinton Ave & Flushing Ave",5043
+"Cadman Plaza West & Montague St",5013
+"Nassau St & Navy St",4766
+"Hancock St & Bedford Ave",4758
+"Gallatin Pl & Livingston St",4690
+"Clermont Ave & Park Ave",4626
+"3 Ave & Schermerhorn St",4575
+"DeKalb Ave & Skillman St",4347
+"Cadman Plaza E & Red Cross Pl",4073
+"Myrtle Ave & St Edwards St",4055
+"Macon St & Nostrand Ave",3875
+"Sands St & Gold St",3692
+"Bedford Ave & S 9th St",3203
+"Franklin Ave & Myrtle Ave",3095
+"Monroe St & Classon Ave",3072
+"Peck Slip & Front Street",2908
+"Hanover Pl & Livingston St",2415
+"Park Ave & St Edwards St",2109
+"7 Ave & Farragut St",1426
+"Railroad Ave & Kay Ave",1046
+"Sands St & Navy St",500
