443eb0c614e90353a8ef44e5c779e396a92c9da3
amit <londhe.amit@gmail.com>
2015-02-21 12:15:19 -0500

    Station cloud with tooltip, spans wrapped properly

--- js/chartdata.js
+++ js/chartdata.js
@@ -149,8 +149,6 @@ quietStations = [
                {label:"Sands St & Navy St", value:500}
 ];
 
-allStation = [
-];
 
 var barChartDataTemplate = {
     labels:[],
@@ -164,3 +162,15 @@ var barChartDataSetTemplate = {
         barStrokeWidth : 1,
         data: []
 };
+
+
+function shuffle(array) {
+	    var input = array;
+	    for (var i = input.length-1; i >=0; i--) {
+	        var randomIndex = Math.floor(Math.random()*(i+1)); 
+	        var itemAtIndex = input[randomIndex]; 
+	        input[randomIndex] = input[i]; 
+	        input[i] = itemAtIndex;
+	    }
+	    return input;
+}
